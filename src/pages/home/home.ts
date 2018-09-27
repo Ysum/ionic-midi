05e542e2bd63d2d54dd128daf7a4fb0b100abb63
lukasmusy <lukasmusy@gmail.com>
2018-09-27 08:30:37 +0200

    init

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { MidiService } from '../../services/MidiService'
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public midiService: MidiService) {
+     midiService.onMidiInit();
   }
 
 }
